4ae6838ff052adfe2f8168428c121d36bcc9a9fd
Ole Morten Amundsen <ole.morten.amundsen@gmail.com>
2016-12-09 00:09:32 +0100

    the god damn tutorials are all wrong. rant rant rant

--- site-cookbooks/paladinci/recipes/default.rb
+++ site-cookbooks/paladinci/recipes/default.rb
@@ -6,12 +6,17 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-node.packages.each do |pkg|
+node.default.packages.each do |pkg|
     package pkg
 end
+
 
 # provision user account
 include_recipe 'user::data_bag'
 
 # provision ssh
 include_recipe 'openssh'
+
+#include_recipe 'ssh-keys'
+#include_recipe 'ssh_known_hosts'
+
